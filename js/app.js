4163c21b1d5266429fd368fe6fcb6636329175fb
nsieber <nsieber@gmail.com>
2015-07-31 13:18:39 -0400

    ryu is throwin down

--- js/app.js
+++ js/app.js
@@ -8,14 +8,26 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
-    $('.hadouken').show();
     $('.ryu-throwing').show();
-    // animate hadouken to the right
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
   })
   .mouseup(function() {
     $('.ryu-ready').show();
     $('.ryu-throwing').hide();
   });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
