427705a39b3606a492ab9ec296bc404018ee28f1
nsieber <nsieber@gmail.com>
2015-08-01 12:27:32 -0400

    get ryu cool pose to work for x key

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
     $('.ryu-still').show();
   })
   .mousedown(function() {
     playHadouken();
+    $('.ryu-still').hide();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
@@ -21,19 +23,24 @@ $(document).ready(function() {
     );
   })
   .mouseup(function() {
-    $('.ryu-ready').show();
     $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
 
-$(document).keydown(function() {
-      $('.ryu-ready').hide()
+$(document).keydown(function(e) {
+	if (e.keyCode == 88) {
+      $('.ryu-ready').hide();
       $('.ryu-still').hide();
       $('.ryu-cool').show();
- }).keyup(function() {
+  }
+  	
+ }).keyup(function(e) {
+ 	if (e.keyCode == 88) {
       $('.ryu-ready').hide();
       $('.ryu-cool').hide();
-      $('.ryu-still').show();      
+      $('.ryu-still').show();
+      }      
 });
 
 function playHadouken () {
